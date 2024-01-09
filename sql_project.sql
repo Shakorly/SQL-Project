b50e0a547be05c9b9aa072c476a1ef00332955bf
Taiwo Shakiru <114835289+Shakorly@users.noreply.github.com>
2024-01-09 22:53:49 +0100

    Update sql_project.sql

--- sql_project.sql
+++ sql_project.sql
@@ -1,3 +1,7 @@
+''' From the following table write a query in SQL to retrieve all rows and columns from the 
+employee table in the Adventureworks database. Sort the result set in ascending order on 
+jobtitle.
+'''
 select 
 * 
 from 
